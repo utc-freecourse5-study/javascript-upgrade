d719b94834a99a23a77ffe2214fd6c702ee0fa8d
chiman <rlaclghks123@naver.com>
2022-12-06 18:42:50 +0900

    feat: 시작문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+const UpgradeGame = require('./UpgradeGame');
+
 class App {
-  play() {}
+  #upgradeGame;
+  constructor() {
+    this.#upgradeGame = new UpgradeGame();
+  }
+  play() {
+    this.#upgradeGame.start();
+  }
 }
+const app = new App();
+app.play();
 
 module.exports = App;
--- src/UpgradeGame.js
+++ src/UpgradeGame.js
@@ -1,3 +1,11 @@
-class UpgradeGame {}
+const OutputView = require('./OutputView');
+
+class UpgradeGame {
+  constructor() {
+    OutputView.printStart();
+  }
+
+  start() {}
+}
 
 module.exports = UpgradeGame;
