d03d0e7cdce5917a3ba30206b8d4526b6bfcf77e
chiman <rlaclghks123@naver.com>
2022-12-07 15:01:52 +0900

    feat: 출력 메세지 상수화

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,30 +1,29 @@
 const { Console } = require('@woowacourse/mission-utils');
+const { OUTPUT_MESSAGE } = require('../utils/message');
 
 const OutputView = {
   printStart() {
-    Console.print('무기 강화 게임을 시작합니다.');
+    Console.print(OUTPUT_MESSAGE.start);
   },
 
   printCurrentUpgradePhase(phase) {
-    Console.print(`\n현재 강화 등급: +${phase}강`);
+    Console.print(OUTPUT_MESSAGE.currentUpgradePhase(phase));
   },
 
   printFinalUpgradePhase(phase) {
-    Console.print(`\n최종 강화 결과: +${phase}강`);
+    Console.print(OUTPUT_MESSAGE.finalUpgradePhase(phase));
   },
 
   printMiniGameSuccess(type, randomNum, bonus) {
-    Console.print(
-      `\n미니 게임 랜덤 수: ${randomNum} --> ${type} 맞추기 성공! 강화 확률 ${bonus}% 증가!`
-    );
+    Console.print(OUTPUT_MESSAGE.miniGameSuccess(type, randomNum, bonus));
   },
 
   printMiniGameFail(type, randomNum) {
-    Console.print(`\n미니 게임 랜덤 수: ${randomNum} --> ${type} 맞추기 실패!`);
+    Console.print(OUTPUT_MESSAGE.miniGameFail(type, randomNum));
   },
 
   printResult(result, bonus) {
-    Console.print(`\n강화 ${result}! (강화 확률 ${bonus}%)`);
+    Console.print(OUTPUT_MESSAGE.gameResult(result, bonus));
   },
 
   printErrorMessage(error) {
