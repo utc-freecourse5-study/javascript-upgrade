a653ae867a1e4a270c549fd2d69acfb30eea1b0c
chiman <rlaclghks123@naver.com>
2022-12-08 18:11:29 +0900

    test: 미니게임 model 분리에 의한 테스트 코드수정

--- __tests__/UpgradeModelTest.js
+++ __tests__/UpgradeModelTest.js
@@ -1,4 +1,6 @@
 const MissionUtils = require('@woowacourse/mission-utils');
+const MiniGame = require('../src/controller/MiniGame');
+const MiniGameModel = require('../src/model/MiniGameModel');
 const UpgradeModel = require('../src/model/UpgradeModel');
 
 const mockRandoms = (numbers) => {
@@ -19,36 +21,37 @@ describe('UpgradeModel Test', () => {
     test('사용자의 입력값이 O일때 홀짝을 맞춘경우 test', () => {
       mockRandoms([7]);
 
-      upgradeModel.makeRandomNumber();
-
-      const result = upgradeModel.isOddAndEven('O');
+      const miniGameModel = new MiniGameModel();
+      new MiniGame(miniGameModel);
+      const result = miniGameModel.isOddAndEven('O');
       expect(result).toBeTruthy();
     });
 
     test('사용자의 입력값이 O일때 홀짝을 틀린경우 test', () => {
       mockRandoms([6]);
 
-      upgradeModel.makeRandomNumber();
-
-      const result = upgradeModel.isOddAndEven('O');
+      const miniGameModel = new MiniGameModel();
+      new MiniGame(miniGameModel);
+      const result = miniGameModel.isOddAndEven('O');
       expect(result).toBeFalsy();
     });
 
     test('사용자의 입력값이 E일때 홀짝을 맞춘경우 test', () => {
       mockRandoms([6]);
 
-      upgradeModel.makeRandomNumber();
+      const miniGameModel = new MiniGameModel();
+      new MiniGame(miniGameModel);
+      const result = miniGameModel.isOddAndEven('E');
 
-      const result = upgradeModel.isOddAndEven('E');
       expect(result).toBeTruthy();
     });
 
     test('사용자의 입력값이 E일때 홀짝을 틀린경우 test', () => {
       mockRandoms([7]);
 
-      upgradeModel.makeRandomNumber();
-
-      const result = upgradeModel.isOddAndEven('E');
+      const miniGameModel = new MiniGameModel();
+      new MiniGame(miniGameModel);
+      const result = miniGameModel.isOddAndEven('E');
       expect(result).toBeFalsy();
     });
   });
@@ -56,27 +59,27 @@ describe('UpgradeModel Test', () => {
   describe('미니게임의 숫자를 맞췄는지 Test', () => {
     test('미니게임 숫자를 맞춘경우 Test', () => {
       mockRandoms([7]);
-
-      upgradeModel.makeRandomNumber();
-      const result = upgradeModel.isCorrectMiniGameNumber(7);
+      const miniGameModel = new MiniGameModel();
+      new MiniGame(miniGameModel);
+      const result = miniGameModel.isCorrectMiniGameNumber(7);
       expect(result).toBeTruthy();
     });
 
     test('미니게임 숫자를 틀린경우 Test', () => {
       mockRandoms([6]);
 
-      upgradeModel.makeRandomNumber();
-      const result = upgradeModel.isCorrectMiniGameNumber(7);
+      const miniGameModel = new MiniGameModel();
+      const result = miniGameModel.isCorrectMiniGameNumber(7);
       expect(result).toBeFalsy();
     });
   });
 
   test('현재 강화 등급 Test', () => {
-    upgradeModel.addUpgradePhase();
-    upgradeModel.addUpgradePhase();
-    upgradeModel.addUpgradePhase();
+    upgradeModel.addUpgradeGrade();
+    upgradeModel.addUpgradeGrade();
+    upgradeModel.addUpgradeGrade();
 
-    const result = upgradeModel.getCurrentUpgradePhase();
+    const result = upgradeModel.getCurrentUpgradeGrade();
     expect(result).toBe(3);
   });
 });
