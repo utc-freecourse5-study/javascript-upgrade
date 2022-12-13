c9d0057951226bc74a2b4b93e4163ec6e67693d4
woo-jk <201702042@o.cnu.ac.kr>
2022-12-14 01:40:49 +0900

    feat: OutputView 최종 결과 출력 함수, 미니 게임 결과 출력 함수 구현

--- src/OutputView.js
+++ src/OutputView.js
@@ -8,6 +8,22 @@ const OutputView = {
   printCurrentLevel(level) {
     MissionUtils.Console.print(`현재 강화 등급: +${level}강`);
   },
+
+  printFinalResult(level) {
+    MissionUtils.Console.print(`최종 강화 결과: +${level}강`);
+  },
+
+  printNumberGameResult(randomNumber, isSuccess) {
+    MissionUtils.Console.print(
+      `미니 게임 랜덤 수: ${randomNumber} --> 숫자 맞추기 ${isSuccess ? "성공! 강화 확률 50% 증가!" : "실패!"}`
+    );
+  },
+
+  printOddGameResult(randomNumber, isSuccess) {
+    MissionUtils.Console.print(
+      `미니 게임 랜덤 수: ${randomNumber} --> 홀/짝 맞추기 ${isSuccess ? "성공! 강화 확률 10% 증가!" : "실패!"}`
+    );
+  },
 };
 
 module.exports = OutputView;
