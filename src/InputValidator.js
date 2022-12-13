942cdf14aef73eaeccf779d0136ca7ba0533cf2b
woo-jk <201702042@o.cnu.ac.kr>
2022-12-14 06:17:06 +0900

    fix: InputValidator 미니 게임 입력 검증 함수 버그 수정

--- src/InputValidator.js
+++ src/InputValidator.js
@@ -9,7 +9,7 @@ const InputValidator = {
     if (!isNaN(input) && (Number(input) < 0 || Number(input) > 9)) {
       throw new Error("[ERROR] 숫자 맞추기 게임은 0~9 사이의 값을 입력해야 합니다.");
     }
-    if (input !== "O" && input !== "E") {
+    if (isNaN(input) && input !== "O" && input !== "E") {
       throw new Error("[ERROR] 홀/짝 맞추기 게임은 O 혹은 E를 입력해야 합니다.");
     }
   },
