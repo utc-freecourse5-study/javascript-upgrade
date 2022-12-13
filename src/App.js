b0455ac460487561de7b3cdaaa265d6d27739756
woo-jk <201702042@o.cnu.ac.kr>
2022-12-14 04:51:05 +0900

    feat: App 초기 프로세스 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+const InputView = require("./InputView");
+const OutputView = require("./OutputView");
+const UpgradeGame = require("./UpgradeGame");
+
 class App {
-  play() {}
+  #UpgradeGame;
+
+  play() {
+    OutputView.printOpening();
+    this.#UpgradeGame = new UpgradeGame();
+  }
+
+  requestChallengeCommand() {
+    InputView.readChallengeCommand((command) => {});
+  }
 }
 
 module.exports = App;
