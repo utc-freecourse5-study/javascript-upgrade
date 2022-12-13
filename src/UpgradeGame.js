fd4a86ee838a72a0afd444c67c9d630f28af5337
woo-jk <201702042@o.cnu.ac.kr>
2022-12-14 03:58:07 +0900

    feat: UpgradeGame 미니 게임 실행 함수들 구현

--- src/UpgradeGame.js
+++ src/UpgradeGame.js
@@ -1,5 +1,35 @@
+const generateMiniGameNumber = require("./generateMiniGameNumber");
+const MiniGame = require("./MiniGame");
+const probability = require("./Probability");
+
 class UpgradeGame {
-  #currentLevel = 0;
+  #level;
+  #probability;
+
+  constructor() {
+    this.#level = 0;
+    this.#probability = probability[this.#level + 1];
+  }
+
+  playOddGame(command) {
+    const randomNumber = generateMiniGameNumber();
+    const miniGame = new MiniGame(randomNumber);
+    const result = miniGame.playOddGame(command);
+
+    if (result) this.#probability += 10;
+
+    return { answer: randomNumber, result: result };
+  }
+
+  playNumberGame(number) {
+    const randomNumber = generateMiniGameNumber();
+    const miniGame = new MiniGame(randomNumber);
+    const result = miniGame.playNumberGame(number);
+
+    if (result) this.#probability += 50;
+
+    return { answer: randomNumber, result: result };
+  }
 }
 
 module.exports = UpgradeGame;
