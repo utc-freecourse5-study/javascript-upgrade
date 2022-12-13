433a62ba3a0c5a2f65a3b9c86b36ad5b38b1497d
woo-jk <201702042@o.cnu.ac.kr>
2022-12-14 03:01:32 +0900

    feat: InputView 강화 도전 여부 입력 함수, 미니 게임 커맨드 입력 함수 구현

--- src/InputView.js
+++ src/InputView.js
@@ -1,9 +1,15 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 const InputView = {
   // 강화 도전 여부
-  readChallengeCommand() {},
+  readChallengeCommand(callback) {
+    MissionUtils.Console.readLine("강화 도전 여부를 입력해주세요. (도전: Y, 중단: N)\n", callback);
+  },
 
   // 미니 게임 숫자 혹은 커맨드
-  readMiniGameInput() {},
+  readMiniGameInput(callback) {
+    MissionUtils.Console.readLine("미니 게임을 위한 숫자 또는 홀/짝 커맨드를 입력해주세요.\n", callback);
+  },
 };
 
 module.exports = InputView;
