c8840556d3d5b034214974f9d144a0b67324df05
woo-jk <201702042@o.cnu.ac.kr>
2022-12-14 01:26:08 +0900

    feat: OutputView 객체 printOpening() 함수, printCurrentLevel() 함수 구현

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,3 +1,13 @@
-const OutputView = {};
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const OutputView = {
+  printOpening() {
+    MissionUtils.Console.print("무기 강화 겡미을 시작합니다.");
+  },
+
+  printCurrentLevel(level) {
+    MissionUtils.Console.print(`현재 강화 등급: +${level}강`);
+  },
+};
 
 module.exports = OutputView;
