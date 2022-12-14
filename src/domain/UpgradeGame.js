3484c96fe212d4bd69e51f87336c809ee9d5220d
woo-jk <201702042@o.cnu.ac.kr>
2022-12-14 19:48:11 +0900

    fix: UpgradeGame 주석 제거

--- src/domain/UpgradeGame.js
+++ src/domain/UpgradeGame.js
@@ -9,36 +9,13 @@ class UpgradeGame {
     this.#weapon = new Weapon();
   }
 
-  // #level;
-  // #probability;
-
-  // constructor() {
-  //   this.#level = 0;
-  //   this.#probability = this.#initProbability();
-  // }
-
   getWeaponLevel() {
     return this.#weapon.getLevel();
   }
 
-  // #initProbability() {
-  //   if (this.#level > 9) return 10;
-
-  //   return probability[this.#level + 1];
-  // }
-
   weaponUpgrade() {
     return this.#weapon.upgrade();
   }
-
-  // #addProbability(probability) {
-  //   if (this.#probability + probability > 100) {
-  //     this.#probability = 100;
-  //     return;
-  //   }
-
-  //   this.#probability += probability;
-  // }
 
   playOddGame(command) {
     const randomNumber = generateMiniGameNumber();
